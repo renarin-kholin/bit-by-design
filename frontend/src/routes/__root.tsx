26d36a16650be45e3b991c4391a941d1de701e88
Sarvesh Kolte <renarinkholin4@proton.me>
2026-01-16 21:57:52 +0530

    feat: add tanstack query

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,8 +1,10 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 
+const queryClient = new QueryClient();
 const RootLayout = () => (
-	<>
+	<QueryClientProvider client={queryClient}>
 		<div className="p-2 flex gap-2">
 			<Link to="/" className="[&.active]:font-bold">
 				Home
@@ -14,7 +16,7 @@ const RootLayout = () => (
 		<hr />
 		<Outlet />
 		<TanStackRouterDevtools />
-	</>
+	</QueryClientProvider>
 );
 
 export const Route = createRootRoute({ component: RootLayout });
