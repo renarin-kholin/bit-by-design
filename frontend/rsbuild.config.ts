e492fce0acd040a231a92ee09ad61e056f35ad88
Sarvesh Kolte <renarinkholin4@proton.me>
2026-01-17 10:53:11 +0530

    chore: add favicons and title

--- frontend/rsbuild.config.ts
+++ frontend/rsbuild.config.ts
@@ -4,11 +4,37 @@ import { tanstackRouter } from "@tanstack/router-plugin/rspack";
 import { pluginCssMinimizer } from "@rsbuild/plugin-css-minimizer";
 
 // https://rsbuild.dev/guide/basic/configure-rsbuild
+{
+  /* <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+<link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+<link rel="shortcut icon" href="/favicon.ico" />
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+<link rel="manifest" href="/site.webmanifest" /> */
+}
 export default defineConfig({
   plugins: [pluginReact(), pluginCssMinimizer()],
   html: {
-    favicon: "src/assets/favicon.ico",
-    title: "Loco SaaS Starter",
+    favicon: "public/favicon.svg",
+    title: "Bit By Design",
+    meta: {
+      description:
+        "Online design competition by IOIT ACM Student Chapter at AISSMS IOIT.",
+    },
+    appIcon: {
+      name: "Bit By Design",
+      filename: "public/site.webmanifest",
+      icons: [
+        {
+          src: "public/favicon-96x96.png",
+          size: 96,
+        },
+        {
+          src: "public/apple-touch-icon.png",
+          target: "apple-touch-icon",
+          size: 180,
+        },
+      ],
+    },
   },
   source: {
     entry: {
--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,6 +1,11 @@
 ///<reference types="@rsbuild/core/types" />
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
+import {
+	createRootRoute,
+	HeadContent,
+	Link,
+	Outlet,
+} from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 const queryClient = new QueryClient();
 
